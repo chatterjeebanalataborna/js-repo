e9c3c38626220388c2125deed547987c1e002f98
chatterjeebanalataborna <124555040+chatterjeebanalataborna@users.noreply.github.com>
2024-08-12 17:16:12 +0000

    datatype.js

---
+++ javascript/datatype.js
@@ -0,0 +1,7 @@
+const accountId=1072;
+let accountname="banalatachatterjee";
+let email="chatterjeebanalata46@gmail.com";
+let accountCity="kolkata";
+let accountState="West Bengal";
+let phone=8276879864;
+console.table([accountId,accountname,email,phone,accountCity,accountState]);
